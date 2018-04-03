d6868a9b81f184940e0868db5ba1a01892e53ff0
alpenfritz <mymailismissing@gmail.com>
2018-04-03 17:25:20 +0900

    Make input a controlled component in SearchBar

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -8,11 +8,14 @@ class SearchBar extends Component {
   }
 
   render () {
+    // by specifying VALUE prop in INPUT element, INPUT becomes a controlled component;
+    // a controlled component has its value set by STATE: its VALUE changes when STATE changes
     return (
       <div>
-        <input onChange={event => this.setState({ term: event.target.value})} />
-        <br />
-        Value of the input: {this.state.term}
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })}
+        />
       </div>
     );
   }
