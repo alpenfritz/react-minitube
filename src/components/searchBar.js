b626df88bc4672cfc5173b8390a84b9a6448ea61
alpenfritz <mymailismissing@gmail.com>
2018-04-03 16:50:48 +0900

    Add state to SearchBar component

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  // STATE = a plaing javascript object that is used to record and react to user events
+  // Whenever a component state is changed, the component immediately renders
+  // and also forces all of its children to render
+  constructor (props) {
+    super(props);
+
+    this.state = { terms: '' };
+  }
+
+
   render () {
     // 2. pass eventHandler to element using prop (ie. onChange)
     return <input onChange={this.onInputChange} />;
