599dc802758e366945cdbadfaeac8fbd00e4f225
alpenfritz <mymailismissing@gmail.com>
2018-04-03 15:19:43 +0900

    Include SearchBar component

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import keys from '../config/keys';
+import SearchBar from './components/searchBar';
 
 const API_KEY = keys.youtubeAPI;
 
 // Create component (this component produces some HTML)
 const App = () => {
-  return <div>Hello World!</div>
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
 };
 
 // Take this component's generated HTML and put it on the page (in the DOM)
