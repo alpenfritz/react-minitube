5cefa178badda8f6b19420b773978deca7192902
alpenfritz <mymailismissing@gmail.com>
2018-04-03 17:11:51 +0900

    Print out state of SearchBar component

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,24 +1,20 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  // STATE = a plaing javascript object that is used to record and react to user events
-  // Whenever a component state is changed, the component immediately renders
-  // and also forces all of its children to render
   constructor (props) {
     super(props);
 
-    this.state = { terms: '' };
+    this.state = { term: '' };
   }
-
 
   render () {
-    // 2. pass eventHandler to element using prop (ie. onChange)
-    return <input onChange={this.onInputChange} />;
-  }
-
-  // 1. create eventHandler (naming convention: on + Element + Event: eg. onInputChange)
-  onInputChange (event) {
-    console.log(event.target.value);
+    return (
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value})} />
+        <br />
+        Value of the input: {this.state.term}
+      </div>
+    );
   }
 };
 
