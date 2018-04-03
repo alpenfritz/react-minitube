90ff7a10a263d29be8f3313e871052f0dbc43f7d
alpenfritz <mymailismissing@gmail.com>
2018-04-03 16:34:30 +0900

    Make SearchBar a class component and add input eventHandler

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-  return <input />;
+class SearchBar extends Component {
+  render () {
+    // 2. pass eventHandler to element using prop (ie. onChange)
+    return <input onChange={this.onInputChange} />;
+  }
+
+  // 1. create eventHandler (naming convention: on + Element + Event: eg. onInputChange)
+  onInputChange (event) {
+    console.log(event.target.value);
+  }
 };
 
 export default SearchBar;
